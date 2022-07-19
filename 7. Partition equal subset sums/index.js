45ce33db9a6cf4f3d6bacf87b61b2eb46d9fc84c
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-07-18 20:28:43 -0700

    commit solution to master

--- 7. Partition equal subset sums/index.js
+++ 7. Partition equal subset sums/index.js
@@ -1,4 +1,5 @@
 /* 
+
 Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
 
 Example 1:
